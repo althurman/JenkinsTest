ffff3daab5b5b6f828076b9bcd89bd7b036154b9
althurman <allen.thurman@perkinelmer.com>
2018-10-25 17:45:42 -0400

    Add files via upload

---
+++ ListFilesBySize/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListFilesBySize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mydir = args[0];
+
+            Console.WriteLine("My dir is: " + mydir);
+
+            var sorted = Directory.GetFiles(mydir, "*.*", System.IO.SearchOption.AllDirectories).OrderBy(f => new FileInfo(f).Length);
+
+            foreach (var f in sorted)
+                Console.WriteLine(f + " " + new FileInfo(f).Length);
+
+
+
+
+        }
+    }
+}
