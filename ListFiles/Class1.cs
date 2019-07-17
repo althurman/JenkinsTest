374844c20c0c75cee8bf137071969d9f36e765c2
Allen Thurman <allen.thurman@perkinelmer.com>
2019-07-17 16:48:42 -0400

    reorg

--- ListFiles/Class1.cs
+++ ListFiles/Class1.cs
@@ -1,17 +1,17 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
-using ListFilesBySize;
+
 
 namespace ListFiles
 {
     public class Class1
     {
-        private ListFilesBySize.Program _otherprog;
+
 
         public Class1()
         {
-            _otherprog = new Program();
+            Console.Write("whatever");
         }
     }
 }
